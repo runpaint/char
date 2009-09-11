3036742384a43d171830bc5956a07ac97de8969a
Run Paint Run Run <runrun@runpaint.org>
2009-09-11 13:22:46 +0100

    Char: Add #property? and #posix_class? predicates

---
+++ lib/char.rb
@@ -0,0 +1,181 @@
+class Char < String
+
+  def initialize(char)
+    char = char.chr('utf-8') if char.is_a?(Fixnum)
+    raise ArgumentError unless (char.is_a?(String) && char.size == 1)
+    super char
+  end
+
+  def letter?
+    property?(:L)
+  end
+
+  def uppercase?
+    property?(:Lu)
+  end
+
+  def lowercase?
+    property?(:Ll)
+  end
+  
+  def titlecase?
+    property?(:Lt)
+  end
+
+  def modifier?
+    property?(:Lm)
+  end
+
+  def alphanumeric?
+    posix_class?(:alnum)
+  end
+
+  def word?
+    posix_class?(:word)
+  end
+
+  def ascii?
+    posix_class?(:ascii)
+  end
+
+  def graph?
+    posix_class?(:graph)
+  end
+
+  def number?
+    property?(:N)
+  end
+
+  def mark?
+    property?(:M)
+  end
+
+  def nonspacing?
+    property?(:Mn)
+  end
+
+  def spacing_combining?
+    property?(:Mc)
+  end
+
+  def enclosing?
+    property?(:Me)
+  end
+
+  def digit?
+    property?(:Nd)
+  end
+
+  def number_letter?
+    property?(:Nl)
+  end
+
+  def hex?
+    posix_class?(:xdigit)
+  end
+
+  def punctuation?
+    property?(:P)
+  end
+
+  def connector?
+    property?(:Pc)
+  end
+
+  def dash?
+    property?(:Pd)
+  end
+
+  def open_punctuation?
+    property?(:Ps)
+  end
+
+  def close_punctuation?
+    property?(:Pe)
+  end
+
+  def initial_punctuation?
+    property?(:Pi)
+  end
+
+  def final_punctuation?
+    property?(:Pf)
+  end
+
+  def symbol?
+    property?(:S)
+  end
+
+  def math?
+    property?(:Sm)
+  end
+
+  def currency?
+    property?(:Sc)
+  end
+
+  def modifier_symbol?
+    property?(:Sk)
+  end
+
+  def separator?
+    property?(:Z)
+  end
+
+  def space?
+    property?(:Zs)
+  end
+
+  def blank?
+    posix_class?(:blank)
+  end
+
+  def line_separator?
+    property?(:Zl)
+  end
+
+  def paragraph_separator?
+    property?(:Zl)
+  end
+
+  def other?
+    property?(:C)
+  end
+
+  def control?
+    property?(:Cc)
+  end
+
+  def format?
+    property?(:Cf)
+  end
+  
+  def surrogate?
+    property?(:Cs)
+  end
+
+  def private?
+    property?(:Co)
+  end
+
+  def unassigned?
+    property?(:Cn)
+  end
+
+  def print?
+    posix_class?(:print)
+  end
+
+  def properties
+    self.class.instance_methods(false).select{|m| m.to_s.end_with?('?')}.
+                                       select{|m| send(m) rescue false }
+  end
+
+  def property?(cat)
+    !!match(/^\p{#{cat.capitalize}}$/u)
+  end
+
+  def posix_class?(cls)
+    !!match(/^[[:#{cls.downcase}:]]$/u)
+  end
+end
--- spec/char_spec.rb
+++ spec/char_spec.rb
@@ -1,7 +1,36 @@
+# coding: utf-8
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Char" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe Char, :initialize do
+  it "accepts a String argument" do
+    Char.new("\t").to_s.should == "\t"
+  end
+
+  it "accepts a Fixnum argument which it treats as a Unicode codepoint" do
+    Char.new(5678).to_s.should == "ᘮ"
+  end
+end
+
+describe Char, :property? do
+  it "returns true if the character is a member of the given 'General category'" do
+    Char.new("c").property?(:Ll).should be_true
+    Char.new(4242).property?(:Nd).should be_true
+  end
+
+  it "returns false if the character is not a member of the given 'General category'" do
+    Char.new("c").property?(:Lu).should be_false
+    Char.new(4242).property?(:So).should be_false
+  end
+end
+
+describe Char, :posix_class? do
+  it "returns true if the character is a member of the given POSIX bracket class" do
+    Char.new(1234).posix_class?(:upper).should be_true
+    Char.new(0x030E).posix_class?(:graph).should be_true
+  end
+
+  it "returns false if the character is not a member of the given POSIX bracket class" do
+    Char.new(1234).posix_class?(:lower).should be_false
+    Char.new(0x030E).posix_class?(:ascii).should be_false
   end
 end
