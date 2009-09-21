f7dfa026717b92a4b8750371c01f8b2feeea45b7
Run Paint Run Run <runrun@runpaint.org>
2009-09-21 21:33:13 +0100

    Char: Remove to-do examples; they now all pass

--- spec/numeric_exhaustive_spec.rb
+++ spec/numeric_exhaustive_spec.rb
@@ -10,8 +10,7 @@ IO.foreach(FILE) do |line|
   parts = line.split(/\s*;\s*/)
   next if parts.empty?
   
-  char = Char.new(parts.first.to_i(16)) rescue nil
-  next unless char && char.exists?
+  char = Char.new(parts.first.to_i(16))
   derived_numeric_values[char] = parts[1..-1]
 end
 
@@ -21,8 +20,6 @@ describe Char, :to_i do
       char.to_i.should == data[0].to_i
     end
   end
-
-  it "understands numeric Han characters"
 end
 
 describe Char, :to_f do
@@ -31,8 +28,6 @@ describe Char, :to_f do
       char.to_f.should == data[2].to_r.to_f
     end
   end
-
-  it "understands numeric Han characters"
 end
 
 describe Char, :to_r do
@@ -41,6 +36,4 @@ describe Char, :to_r do
       char.to_r.should == data[2].to_r
     end
   end
-
-  it "understands numeric Han characters"
 end
