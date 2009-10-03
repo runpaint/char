662020d764ae78523dd8f1ff22a0f6219c9e44fb
Run Paint Run Run <runrun@runpaint.org>
2009-10-03 22:55:24 +0100

    Char#bidi_control?: Update to use boolean DSL

--- lib/char/bidi-c.rb
+++ lib/char/bidi-c.rb
@@ -1,10 +1,6 @@
-class Char::BidiControl
-  DEFAULT = false
-  @@bidic = {true=>[8206..8207, 8234..8238]}
-  def self.[](codepoint)
-    @@bidic.each_pair do |bool,ranges|
-      return bool if ranges.any?{|rng| rng.include?(codepoint)}
-    end
-    DEFAULT
-  end
+require_relative 'data'
+
+class Char::BidiControl < Char::Data::Boolean
+  default false
+  codepoints [8206..8207, 8234..8238]
 end
