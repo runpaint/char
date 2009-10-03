14752f0fd906574d2030bbd9967effb268edea69
Run Paint Run Run <runrun@runpaint.org>
2009-10-03 22:39:07 +0100

    Char#whitespace?: Update to use boolean DSL

--- lib/char/wspace.rb
+++ lib/char/wspace.rb
@@ -1,7 +1,4 @@
-class Char::WhiteSpace
-  DEFAULT = false
-  NON_DEFAULT = [9..13, 32..32, 133..133, 160..160, 5760..5760, 6158..6158, 8192..8202, 8232..8233, 8239..8239, 8287..8287, 12288..12288]
-  def self.[](codepoint)
-    NON_DEFAULT.any?{|rng| rng.include?(codepoint)} ? !DEFAULT : DEFAULT
-  end
+class Char::WhiteSpace < Char::Data::Boolean
+  default false
+  codepoints [9..13, 32..32, 133..133, 160..160, 5760..5760, 6158..6158, 8192..8202, 8232..8233, 8239..8239, 8287..8287, 12288..12288]
 end
